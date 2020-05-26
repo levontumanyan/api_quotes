27a804c00f2a101d4abbcd009416c62388cc71db
Levon T <itfibonacc@live.com>
2020-05-26 13:54:46 -0400

    fixed the MONGODB_URI config var in code and setttings

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(express.json()); // Make sure it comes back as json
 //     useNewUrlParser: true
 // });
 
-const connection = "mongodb+srv://api_quotes_admin:Zs3w73dqMjTiN2N@quotesapi01-s2dqb.mongodb.net/test?retryWrites=true&w=majority";
+const connection = process.env.MONGODB_URI || "mongodb+srv://api_quotes_admin:Zs3w73dqMjTiN2N@quotesapi01-s2dqb.mongodb.net/test?retryWrites=true&w=majority";
 
-mongoose.connect(process.env.MONGODB_URI || connection,
+mongoose.connect(connection,
     { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
