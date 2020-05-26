1a53865250ceb7a60a1ba8d3be99d5ad30e45bdd
Levon T <itfibonacc@live.com>
2020-05-26 12:56:01 -0400

    fixed the port environment variable

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const quote_routes = require('./routes/quote_routes.js');
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
