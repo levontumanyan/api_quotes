f8d2f4ad792bbf992e936579f1ed5c8e1f218839
Levon T <itfibonacc@live.com>
2020-06-08 17:37:12 -0400

    Added upvoting/downvoting functionality

---
+++ routes/post_votes.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var router = express.Router();
+const quoteModel = require('../models/quote_model');
+var morgan = require('morgan');
+
+router.use(morgan('tiny'));
+
+router.get('/:id', async function (req, res) {
+
+    const quote_to_rate = await quoteModel.findById(req.params.id);
+    //console.log(quote[0]);
+
+    try {
+        // quote_body = quote.map((document) => document.body)
+        res.send(quote_to_rate);
+        console.log(quote_to_rate);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+
+});
+
+
+router.post('/:id/upvote', async function (req, res) {
+
+    const quote_to_rate = await quoteModel.findById(req.params.id);
+    const num_of_upvotes = await quote_to_rate.meta.upvotes;
+    const num_of_downvotes = await quote_to_rate.meta.downvotes;
+
+    console.log(num_of_upvotes);
+
+    const query = { _id: req.params.id };
+    const update = { meta: { upvotes: num_of_upvotes + 1, downvotes: num_of_downvotes } };
+
+    await quoteModel.findOneAndUpdate(query, update, function (err, result) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+router.post('/:id/downvote', async function (req, res) {
+
+    const quote_to_rate = await quoteModel.findById(req.params.id);
+    const num_of_upvotes = await quote_to_rate.meta.upvotes;
+    const num_of_downvotes = await quote_to_rate.meta.downvotes;
+
+    console.log(num_of_downvotes);
+
+    const query = { _id: req.params.id };
+    const update = { meta: { upvotes: num_of_upvotes, downvotes: num_of_downvotes + 1 } };
+
+    await quoteModel.findOneAndUpdate(query, update, function (err, result) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const quote_routes = require('./routes/quote_routes.js');
+var post_votes = require('./routes/post_votes.js')
 
 
 
@@ -22,6 +23,8 @@ mongoose.connect(connection,
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
 
+app.use('/rate_quote', post_votes);
 app.use(quote_routes);
+
 
 app.listen(PORT, () => { console.log(`Server is running on port: ${PORT}`) });
